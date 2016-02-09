360e6ffe9062feb48870c616dc7e13176141fecf
= <=>
2016-02-09 12:03:57 +0000

    Added ini and loadedModulesInfo in the Debug/Module.php file

--- module/Debug/Module.php
+++ module/Debug/Module.php
@@ -9,8 +9,31 @@
 namespace Debug;
 
 
+use Zend\EventManager\Event;
+use Zend\ModuleManager\ModuleEvent;
+use Zend\ModuleManager\ModuleManager;
+
 class Module
 {
+    /**
+     * @param ModuleManager $moduleManager
+     */
+    public function init(ModuleManager $moduleManager)
+    {
+        $eventManager = $moduleManager->getEventManager();
+        $eventManager->attach(ModuleEvent::EVENT_LOAD_MODULES_POST, array($this, 'loadedModulesInfo'));
+    }
+
+
+    /**
+     *
+     */
+    public function loadedModulesInfo(Event $event)
+    {
+        $moduleManager = $event->getTarget();
+        $loadedModules = $moduleManager->getLoadedModules();
+        error_log(var_export($loadedModules, true));
+    }
 
     /**
      *
